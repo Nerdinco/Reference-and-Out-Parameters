6019a4ed935d4d393d4aeec29560cb61a8b5e0ca
Nerdinco <kelvinmich13@gmail.com>
2022-06-28 17:24:10 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace RefAndOut {
+class Program{
+    static void TestFunc1(int arg1 ){
+        arg1+=10;
+        // arg1 = arg1 + 10
+        Console.WriteLine($"{arg1}");
+    }
+
+    static void TestFunc2(ref int arg1){
+        arg1+=10;
+        Console.WriteLine($"{arg1}");
+    }
+    static void Main (string[]args){
+        int val1=10;
+        int val2=20;
+        // TestFunc1(val1);
+        
+        Console.WriteLine($"{val1}");  
+         TestFunc2(ref val1);
+         Console.WriteLine($"{val1}"); 
+
+         
+    }
+    static void PlusTimes()
+    int a,b;
+         PlusTimes(val1,val2,out a,out b);
+         Console.WriteLine($"{a},{b}");
+}
+}
